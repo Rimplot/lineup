a0cff2371b170d98bb2a7bde86ea4d2496941c1f
Tamás Sándor <tamas.sandor@bluesoft.cz>
2023-05-18 13:22:03 +0200

    firestore local cache

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,7 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import {
+	getFirestore,
+	initializeFirestore,
+	persistentLocalCache
+} from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -18,6 +22,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore();
+const db = initializeFirestore(app, {
+	localCache: persistentLocalCache(/*settings*/ {})
+});
 
 export { auth, db };
